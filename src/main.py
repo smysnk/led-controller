6f5b72fa80e47ce7f5f89cf6327975faf9f8442b
Joshua Bellamy <josh@psidox.com>
2021-01-11 09:20:01 -0700

    Project init.

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import neopixel
+from machine import Pin
+# from adafruit_led_animation.animation.blink import Blink
+# import adafruit_led_animation.color as color
+
+from adafruit_led_animation.animation.blink import Blink
+from adafruit_led_animation.animation.comet import Comet
+from adafruit_led_animation.animation.chase import Chase
+from adafruit_led_animation.sequence import AnimationSequence
+from adafruit_led_animation.color import PURPLE, AMBER, JADE
+
+def start(env=None, requests=None, logger=None, time=None, updater=None):
+
+  # Update to match the pin connected to your NeoPixels
+  # pixel_pin = board.D6
+  # Update to match the number of NeoPixels you have connected
+  num_pixels = 100
+  pixel_pin = 26
+  pixels = neopixel.NeoPixel(Pin(pixel_pin, Pin.OUT), num_pixels)
+
+  # pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=0.5, auto_write=False)
+
+  blink = Blink(pixels, speed=0.5, color=JADE)
+  comet = Comet(pixels, speed=0.01, color=PURPLE, tail_length=10, bounce=True)
+  # chase = Chase(pixels, speed=0.1, size=3, spacing=6, color=AMBER)
+
+  animations = AnimationSequence(blink, comet, advance_interval=2, auto_clear=True)
+
+  while True:
+    animations.animate()
+
+start()
